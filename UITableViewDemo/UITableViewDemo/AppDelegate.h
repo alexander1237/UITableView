482fcb94dee4011a849341dc6f3d969a1b844994
Jack <sunzili00@hotmail.com>
2015-01-12 21:16:59 +0800

    [uitableview doc]init

---
+++ UITableViewDemo/UITableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewDemo
+//
+//  Created by jack on 15/1/12.
+//  Copyright (c) 2015年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewDemo/UITableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewDemo
+//
+//  Created by jack on 15/1/12.
+//  Copyright (c) 2015年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
